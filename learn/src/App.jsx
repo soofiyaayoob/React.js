b80b981fb88d8a73647678a7232c099d32f20f49
soofiya <soofiyaayoob786@gmail.com>
2025-03-26 20:40:34 +0530

    Introductory to React Learning

---
+++ learn/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./header";
+import Footer from "./Footer";
+import Food from "./food";
+function App() {
+ return(
+  <>
+  <Header/>
+  <Footer/>
+  <Food/>
+  </>
+
+ );
+}
+
+export default App
